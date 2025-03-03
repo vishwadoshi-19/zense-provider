6be708132b458c1bdab3e646f9671313b3a07861
VISHWA SMITEN DOSHI <99802964+vishwadoshi-19@users.noreply.github.com>
2025-03-03 22:35:00 +0530

    error resolving 5

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
