ad66dc5f548322ad9b9d619594f47021c90e99fd
VISHWA SMITEN DOSHI <99802964+vishwadoshi-19@users.noreply.github.com>
2025-03-03 22:54:11 +0530

    error resolving 6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,12 @@ import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
+    <>
       <Head>
-        <title>Nurch Provider Dashboard</title>
+        <title>Zense Provider Dashboard</title>
         <meta
           name="description"
-          content="Nurch Provider Dashboard for managing staff and jobs"
+          content="Zense Provider Dashboard for managing staff and jobs"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
@@ -20,6 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Component {...pageProps} />
       <OfflineAlert />
-    </AuthProvider>
+    </>
   );
 }
